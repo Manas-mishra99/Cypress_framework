71f91d61e48151fd33df7106ee192262029b10da
Manas-mishra99 <mishramanas428@gmail.com>
2024-10-16 23:09:39 +0530

    UI element Test

--- cypress/Integration/examples/Test2.js
+++ cypress/Integration/examples/Test2.js
@@ -14,9 +14,30 @@ describe('Handel UI Web Elements', () => {
 
     })
     it('Drop Down Elements', () => {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
         //Static drop down elements
+        cy.get('select').select('option2').should('have.value', 'option2');
+
+        //Dynamic drop down elements
+        cy.get('#autocomplete').type('Ind');
+        cy.get('.ui-menu-item div').each(($ele, index, $list) => {
+            if ($ele.text() === "India") {
+                cy.wrap($ele).click();
+            }
+        })
+        cy.get('#autocomplete').should('have.value', 'India');
+
 
 
     })
-
+    it('visible Elements ', () => {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        cy.get('#displayed-text').should('be.visible');
+        cy.get('#hide-textbox').click();
+        cy.get('#displayed-text').should('not.be.visible');
+    })
+    it('Radio Button ', () => {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        cy.get('input[type="radio"]').check(['radio3']).should('be.checked');
+    })
 })
